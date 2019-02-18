5192f66b9ddde17421cf11a6ee355dd082ff96d1
unknown <ankukd1995@gmail.com>
2019-02-18 15:15:26 +0530

    This is the first commit of the project

---
+++ src/Automation/AutosuggestionsSelections.java
@@ -0,0 +1,17 @@
+package Automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AutosuggestionsSelections 
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","F:\\Selenium\\\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+	}
+
+}
